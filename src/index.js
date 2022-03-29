a7398bdd5e0c2d85b297552b6c01049db5c7e37a
marvinscheffold <m@marvinscheffold.com>
2022-03-29 18:10:36 +0200

    initial setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const menu = [
+    { name: "Hamburger Royal TS", calories: 515 },
+    { name: "Milkshake Schokogeschmack", calories: 440 },
+    { name: "Big Tasty Bacon", calories: 905 },
+    { name: "Hamburger", calories: 255 },
+    { name: "Fruchttüte", calories: 45 },
+    { name: "6 Chicken McNuggets", calories: 250 },
+    { name: "Big Mac", calories: 495 },
+    { name: "Pommes Frites groß", calories: 470 },
+];
